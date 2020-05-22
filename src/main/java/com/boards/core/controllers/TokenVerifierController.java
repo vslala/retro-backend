8719fd3b1e4495c0174224a90b43c4c3e5d78081
Varun Shrivastava <Varun.Shrivastava@thoughtworks.com>
2020-05-22 14:26:19 +0530

    add token verifier controller to validate the token

---
+++ src/main/java/com/boards/core/controllers/TokenVerifierController.java
@@ -0,0 +1,36 @@
+package com.boards.core.controllers;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import lombok.extern.log4j.Log4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.Objects;
+
+@CrossOrigin
+@Log4j
+@RestController
+@RequestMapping("/token")
+public class TokenVerifierController {
+
+    @GetMapping("/verify")
+    public ResponseEntity<HttpStatus> verifyToken(@RequestParam("id_token") String idTokenString) {
+        if (Objects.isNull(idTokenString)) return ResponseEntity.badRequest().build();
+
+        FirebaseToken idToken = null;
+        try {
+            idToken = FirebaseAuth.getInstance().verifyIdToken(idTokenString);
+        } catch (FirebaseAuthException e) {
+            log.info("Invalid token!");
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok()
+                .header("Authorization", idTokenString)
+                .build();
+    }
+}
---
+++ src/main/java/com/boards/core/model/UserBoard.java
@@ -0,0 +1,32 @@
+package com.boards.core.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "user_boards")
+@IdClass(UserBoard.UserBoardCompositeId.class)
+public class UserBoard {
+
+    @Id
+    @Column(name = "uid")
+    private String uid;
+
+    @Id
+    @Column(name = "board_id")
+    private String boardId;
+
+    @Data
+    public static class UserBoardCompositeId implements Serializable {
+        private String uid;
+        private String boardId;
+
+        public UserBoardCompositeId(String uid, String boardId) {
+            this.uid = uid;
+            this.boardId = boardId;
+        }
+    }
+}
