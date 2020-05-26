fcce9f3f93fb7dd847184425521eef70637047c1
Varun Shrivastava <varunshrivastava007@gmail.com>
2020-05-26 21:59:23 +0530

    add ci/cd pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
-./gradlew clean build -x test
-docker system prune -f
-docker images
-docker rmi vslala/retro-board-backend
-docker build -t vslala/retro-board-backend:latest .
+export MYSQL_CONTAINER_NAME=$1
+export IMAGE_REPOSITORY=$2
+export IMAGE_TAG=$3
+export MYSQL_ROOT_PASSWORD=$4
+export MYSQL_USER=$5
+export MYSQL_PASS=$6
+export SPRING_DATASOURCE_URL="jdbc:mysql://${MYSQL_CONTAINER_NAME}:3306/retroboarddb?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&autoReconnect=true"
+export SPRING_DATASOURCE_USERNAME=${MYSQL_USER}
+export SPRING_DATASOURCE_PASSWORD=${MYSQL_PASS}
+export CREDENTIAL_FILE_NAME=$7
+export SPRING_ACTIVE_PROFILE=$8
+docker-compose config
+docker-compose pull
 docker-compose up
-
