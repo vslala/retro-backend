bcb3cbc1de056afdc5e5790810c401b3c51f1c31
Varun Shrivastava <varunshrivastava007@gmail.com>
2020-05-28 01:12:09 +0530

    externalize mysql port

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
+sudo docker images | grep "registry.gitlab.com" | xargs sudo docker rmi
 export MYSQL_CONTAINER_NAME=$1
 export IMAGE_REPOSITORY=$2
 export IMAGE_TAG=$3
 export MYSQL_ROOT_PASSWORD=$4
 export MYSQL_USER=$5
 export MYSQL_PASS=$6
-export SPRING_DATASOURCE_URL="jdbc:mysql://${MYSQL_CONTAINER_NAME}:3306/retroboarddb?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&autoReconnect=true"
+export SPRING_DATASOURCE_URL="jdbc:mysql://${MYSQL_CONTAINER_NAME}:${MYSQL_PORT}/retroboarddb?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&autoReconnect=true"
 export SPRING_DATASOURCE_USERNAME=${MYSQL_USER}
 export SPRING_DATASOURCE_PASSWORD=${MYSQL_PASS}
 export CREDENTIAL_FILE_NAME=$7
