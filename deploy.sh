acc75d26ad2134d840702493c47aa433a0a0c83d
Varun Shrivastava <varunshrivastava007@gmail.com>
2020-05-28 01:24:16 +0530

    pass mysql_port as a variable to deploy script to be set  as env variable

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ export IMAGE_TAG=$3
 export MYSQL_ROOT_PASSWORD=$4
 export MYSQL_USER=$5
 export MYSQL_PASS=$6
+export MYSQL_PORT=$7
 export SPRING_DATASOURCE_URL="jdbc:mysql://${MYSQL_CONTAINER_NAME}:${MYSQL_PORT}/retroboarddb?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&autoReconnect=true"
 export SPRING_DATASOURCE_USERNAME=${MYSQL_USER}
 export SPRING_DATASOURCE_PASSWORD=${MYSQL_PASS}
-export CREDENTIAL_FILE_NAME=$7
-export SPRING_ACTIVE_PROFILE=$8
+export CREDENTIAL_FILE_NAME=$8
+export SPRING_ACTIVE_PROFILE=$9
 docker-compose config
 docker-compose pull
 docker-compose up
