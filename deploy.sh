21d5b711319eb2d90e98e071c20c41030c6624c5
Varun Shrivastava <varunshrivastava007@gmail.com>
2020-05-28 10:30:46 +0530

    delete containers and images before startup

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,9 @@ export MYSQL_PASS=$6
 export MYSQL_PORT=$7
 export SPRING_DATASOURCE_URL="jdbc:mysql://${MYSQL_CONTAINER_NAME}:${MYSQL_PORT}/retroboarddb?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&autoReconnect=true"
 export SPRING_DATASOURCE_USERNAME=${MYSQL_USER}
-export SPRING_DATASOURCE_PASSWORD=${MYSQL_PASS}
+export SPRING_DATASOURCE_PASSWORD=${MYSQL_ROOT_PASSWORD}
 export CREDENTIAL_FILE_NAME=$8
 export SPRING_ACTIVE_PROFILE=$9
 docker-compose config
 docker-compose pull
-docker-compose up
-sudo docker images | grep "registry.gitlab.com" | xargs sudo docker rmi
+docker-compose up -d
