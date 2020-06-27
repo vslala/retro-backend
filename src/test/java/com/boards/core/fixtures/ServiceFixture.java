96b82b3c852da76d0b9ec92ff7b9bf71164e4a94
Varun Shrivastava <varunshrivastava007@gmail.com>
2020-06-27 12:35:36 +0530

    test coverage for RetroWallsService

--- src/test/java/com/boards/core/fixtures/ServiceFixture.java
+++ src/test/java/com/boards/core/fixtures/ServiceFixture.java
@@ -1,19 +1,24 @@
 package com.boards.core.fixtures;
 
-import com.boards.core.model.dto.retroboard.RetroBoardRequest;
-import com.boards.core.model.entities.retroboard.RetroBoard;
-import com.boards.core.model.entities.retroboard.User;
+import com.boards.core.model.dto.retroboard.*;
+import com.boards.core.model.entities.retroboard.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Arrays;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ServiceFixture {
 
+
     private ServiceFixture() {}
     public static final String RETRO_BOARD_ID = "retro-board-id";
+    public static final String WALL_ID = "wall-id";
+    public static final String WALL_STYLE_ID = "wall-style-id";
+    public static final String STICKY_NOTE_STYLE_ID = "sticky-note-style-id";
 
     public static RetroBoardRequest buildRetroBoardRequest() {
         RetroBoardRequest retroBoardRequest = new RetroBoardRequest();
@@ -52,5 +57,68 @@ public class ServiceFixture {
         loggedInUser.setEmail("johndoe@gmail.com");
         loggedInUser.setUsername("johndoe");
         return loggedInUser;
+    }
+
+    public static CreateRetroWallsRequest buildRetroWallsRequest() {
+        CreateRetroWallsRequest retroWallsRequest = new CreateRetroWallsRequest();
+        retroWallsRequest.setRetroBoardId(RETRO_BOARD_ID);
+
+        RetroWallRequest retroWallRequest = buildRetroWallRequest();
+        retroWallsRequest.setWalls(Arrays.asList(retroWallRequest));
+        return retroWallsRequest;
+    }
+
+    public static RetroWallRequest buildRetroWallRequest() {
+        var retroWallRequest = new RetroWallRequest();
+        retroWallRequest.setRetroBoardId(RETRO_BOARD_ID);
+        retroWallRequest.setSortCards(false);
+        retroWallRequest.setTitle("Foo Bar");
+        retroWallRequest.setWallId(WALL_ID);
+        retroWallRequest.setWallOrder(1);
+        retroWallRequest.setStyle(buildWallStyleRequest());
+        return retroWallRequest;
+    }
+
+    public static WallStyleRequest buildWallStyleRequest() {
+        WallStyleRequest wallStyleRequest = new WallStyleRequest();
+        wallStyleRequest.setStickyNote(buildStickyNoteStyleRequest());
+        return wallStyleRequest;
+    }
+
+    public static CreateStickyNoteStyleRequest buildStickyNoteStyleRequest() {
+        CreateStickyNoteStyleRequest stickyNoteRequest = new CreateStickyNoteStyleRequest();
+        stickyNoteRequest.setBackgroundColor("green");
+        stickyNoteRequest.setLikeBtnPosition("right");
+        stickyNoteRequest.setNoteStyleId("note-style-id");
+        stickyNoteRequest.setTextColor("white");
+        return stickyNoteRequest;
+    }
+
+    public static RetroWall buildPersistedRetroWall() {
+        RetroWall persistedRetroWall = new RetroWall();
+        persistedRetroWall.setRetroBoardId(RETRO_BOARD_ID);
+        persistedRetroWall.setSortCards(false);
+        persistedRetroWall.setTitle("Foo Bar");
+        persistedRetroWall.setWallId(WALL_ID);
+        persistedRetroWall.setWallOrder(1);
+        persistedRetroWall.setWallStyle(WALL_STYLE_ID);
+        return persistedRetroWall;
+    }
+
+    public static StickyNoteStyle buildPersistedStickyNoteStyle() {
+        var stickyNoteStyle = new StickyNoteStyle();
+        stickyNoteStyle.setBackgroundColor("crimson");
+        stickyNoteStyle.setLikeBtnPosition("right");
+        stickyNoteStyle.setTextColor("white");
+        stickyNoteStyle.setWallStyleId(WALL_STYLE_ID);
+        stickyNoteStyle.setStickyNoteStyleId(STICKY_NOTE_STYLE_ID);
+        return stickyNoteStyle;
+    }
+
+    public static WallStyle buildPersistedWallStyle() {
+        var wallStyle = new WallStyle();
+        wallStyle.setWallId(WALL_ID);
+        wallStyle.setWallStyleId(WALL_STYLE_ID);
+        return wallStyle;
     }
 }
---
+++ src/test/java/com/boards/core/services/RetroWallsServiceTest.java
@@ -0,0 +1,57 @@
+package com.boards.core.services;
+
+import com.boards.core.model.dto.retroboard.RetroWallsResponse;
+import com.boards.core.model.repositories.retroboard.RetroBoardRepository;
+import com.boards.core.model.repositories.retroboard.RetroWallRepository;
+import com.boards.core.model.repositories.retroboard.StickyNoteStyleRepository;
+import com.boards.core.model.repositories.retroboard.WallStyleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.util.Arrays;
+
+import static com.boards.core.fixtures.ServiceFixture.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class RetroWallsServiceTest {
+
+    private RetroWallRepository retroWallRepository;
+    private StickyNoteStyleRepository stickyNoteStyleRepository;
+    private WallStyleRepository wallStyleRepository;
+    private RetroBoardRepository retroBoardRepository;
+    private RetroWallsService retroWallsService;
+
+    @BeforeEach
+    public void setup() {
+        retroWallRepository = mock(RetroWallRepository.class);
+        stickyNoteStyleRepository = mock(StickyNoteStyleRepository.class);
+        wallStyleRepository = mock(WallStyleRepository.class);
+        retroBoardRepository = mock(RetroBoardRepository.class);
+        retroWallsService = new RetroWallsService(retroWallRepository, stickyNoteStyleRepository,
+                wallStyleRepository, retroBoardRepository);
+    }
+
+    @Test
+    public void itShouldReturnUriForTheBoardIfTheWallsHaveBeenInitializedBefore() {
+        when(retroWallRepository.findAllByRetroBoardId(RETRO_BOARD_ID)).thenReturn(Arrays.asList(buildPersistedRetroWall()));
+        URI uri = retroWallsService.createWalls(buildRetroWallsRequest());
+        assertEquals("/retro-board/walls/" + RETRO_BOARD_ID, uri.toString());
+    }
+
+    @Test
+    public void itShouldGetRetroWallsResponseByRetroBoardId() {
+        when(retroWallRepository.findAllByRetroBoardId(RETRO_BOARD_ID)).thenReturn(Arrays.asList(buildPersistedRetroWall()));
+        when(wallStyleRepository.findAllByWallIdIn(Arrays.asList(WALL_ID))).thenReturn(Arrays.asList(buildPersistedWallStyle()));
+        when(stickyNoteStyleRepository.findAllByWallStyleIdIn(Arrays.asList(WALL_STYLE_ID))).thenReturn(Arrays.asList(buildPersistedStickyNoteStyle()));
+
+        RetroWallsResponse response = retroWallsService.getWallsForBoard(RETRO_BOARD_ID);
+
+        assertEquals(RETRO_BOARD_ID, response.getRetroBoardId());
+        assertEquals(1, response.getWalls().size());
+        assertEquals("right", response.getWalls().get(0).getStyle().getStickyNote().getLikeBtnPosition());
+    }
+
+}
