9d0548534fb2b4368604240e3d599022112ea1df
Varun <varun.shrivastava@klarna.com>
2021-05-30 20:42:18 +0200

    add security token to SwaggerUI to make authenticated requests for testing

--- src/main/java/com/boards/core/configuration/SwaggerConfig.java
+++ src/main/java/com/boards/core/configuration/SwaggerConfig.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @EnableSwagger2
 @Configuration
@@ -15,9 +21,21 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(List.of(SecurityContext.builder().securityReferences(defaultAuth()).build()))
+                .securitySchemes(List.of(apiKeys()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.boards.core.controllers"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        var authorizationScope = new AuthorizationScope("global", "Access main APIs");
+        var authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKeys() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 }
